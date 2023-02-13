781f0a443f1204f5b6d9f1c82136ef491a078714
drduhuuk <drduhuuk@users.noreply.github.com>
2023-02-13 15:55:41 +0000

    Upload

---
+++ LJMU houses/app-files/data.js
@@ -0,0 +1,383 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-modular-houses-front",
+      "name": "Modular houses Front",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.13785825439747867,
+          "pitch": -0.07091381419557052,
+          "rotation": 0,
+          "target": "1-ljmu-houses-front-left"
+        },
+        {
+          "yaw": 0.910290322964924,
+          "pitch": -0.06021952829393129,
+          "rotation": 0,
+          "target": "3-modular-houses-side"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-ljmu-houses-front-left",
+      "name": "LJMU houses Front left",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.661680419389949,
+          "pitch": 0.07445793183474692,
+          "rotation": 0,
+          "target": "2-ljmu-houses-front-right"
+        },
+        {
+          "yaw": 1.2255027687163889,
+          "pitch": -0.1200868792412777,
+          "rotation": 0,
+          "target": "4-ljmu-houses-side"
+        },
+        {
+          "yaw": 1.944008701815524,
+          "pitch": -0.11616112544171031,
+          "rotation": 0,
+          "target": "3-modular-houses-side"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-ljmu-houses-front-right",
+      "name": "LJMU houses Front right",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.8861853016280925,
+          "pitch": 0.10065364099844842,
+          "rotation": 0,
+          "target": "1-ljmu-houses-front-left"
+        },
+        {
+          "yaw": 0.38264059312980336,
+          "pitch": -0.055726724860573285,
+          "rotation": 0,
+          "target": "4-ljmu-houses-side"
+        },
+        {
+          "yaw": 2.2709470348287653,
+          "pitch": 0.12297566503260171,
+          "rotation": 0,
+          "target": "3-modular-houses-side"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-modular-houses-side",
+      "name": "Modular houses side",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.1393904938610255,
+          "pitch": 0.08867796946763384,
+          "rotation": 0,
+          "target": "2-ljmu-houses-front-right"
+        },
+        {
+          "yaw": 1.6549322980218317,
+          "pitch": 0.012761434780346192,
+          "rotation": 0,
+          "target": "4-ljmu-houses-side"
+        },
+        {
+          "yaw": -1.1949771071078956,
+          "pitch": 0.11917858789026425,
+          "rotation": 0,
+          "target": "0-modular-houses-front"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-ljmu-houses-side",
+      "name": "LJMU houses side",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.5981860975209745,
+          "pitch": 0.11616575218834768,
+          "rotation": 0,
+          "target": "3-modular-houses-side"
+        },
+        {
+          "yaw": -1.072960608275439,
+          "pitch": 0.15188695079517345,
+          "rotation": 0,
+          "target": "2-ljmu-houses-front-right"
+        },
+        {
+          "yaw": 1.4179624404558364,
+          "pitch": 0.0609262580054839,
+          "rotation": 0,
+          "target": "6-ljmu-houses-sunflower"
+        },
+        {
+          "yaw": 0.7320528188030302,
+          "pitch": 0.008323483285533229,
+          "rotation": 0,
+          "target": "5-ljmu-houses-back"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-ljmu-houses-back",
+      "name": "LJMU houses back",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.143221223496413,
+          "pitch": 0.08790578923303372,
+          "rotation": 0,
+          "target": "6-ljmu-houses-sunflower"
+        },
+        {
+          "yaw": -1.0454224808942847,
+          "pitch": 0.08496031637396229,
+          "rotation": 0,
+          "target": "4-ljmu-houses-side"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-ljmu-houses-sunflower",
+      "name": "LJMU houses sunflower",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.0123125769980863,
+          "pitch": 0.1285651079689636,
+          "rotation": 0,
+          "target": "5-ljmu-houses-back"
+        },
+        {
+          "yaw": -1.683839239504909,
+          "pitch": 0.15859549984993926,
+          "rotation": 0,
+          "target": "4-ljmu-houses-side"
+        },
+        {
+          "yaw": -1.4396045985198658,
+          "pitch": 0.045857273774396745,
+          "rotation": 0,
+          "target": "3-modular-houses-side"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
